b69f2f287696f291d063f19afb54ab9d5e38da76
yashnathe <yashnathe360@gmail.com>
2023-04-29 15:56:09 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Angular_Assignment_13';
+  firstName: string = "";
+  lastName: string = "";
+  email: string = "";
+  phone: string = "";
+  address: string = "";
+
+  submitForm() {
+    // handle form submission here
+  }
 }
